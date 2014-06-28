e71a5cb0f3bbd87effb1a4ce5620cbe0cace91e1
Zachary Perlmutter <zrp200@gmail.com>
2014-06-28 14:46:31 -0700

    Uploaded first batch of sentences

---
+++ lib/fruitfacts.rb
@@ -0,0 +1,20 @@
+# Produces a random fact.
+def fact
+  [
+    "The average strawberry has around 200 seeds.",
+    "Peaches were once known as Persian apples.",
+    "Add half a cup of lemon juice to your laundry to brighten your whites.",
+    "Kiwi fruits were originally known by their Chinese name, yang tao (sunny peach).",
+    "Strawberries are members of the rose family.",
+    "The inside of banana peel can be used to polish leather shoes.",
+    "Apples have almost 5x the antioxidant capicity of bananas.",
+    "Kiwi fruits are declared as the national fruit of China.",
+    "In China, peaches are a symbol of longevity & good luck.",
+    "There are almost 400 varieties the inside of a banana skin on a mosquito bite to stop the itchiness.",
+    "Bananas are high in the B-complex vitamins, which help calm the nervous system."
+    "If you warm a lemon before squeezing, it will yield much more juice!",
+    "Passion fruits were first grown in Brazil."
+    "Mature lemon trees may prduce around 1,500 fruits per year."
+    "Pineapples contain bromelain, which is used to stop cold beer from going cloudy."
+  ].sample
+end
